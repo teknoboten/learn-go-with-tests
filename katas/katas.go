5247b3426245f716adee2e985b8ed1259e1d8341
serra <serra@boten.ca>
2023-04-19 14:18:36 -0700

    add abbrevname kata

--- katas/katas.go
+++ katas/katas.go
@@ -1,8 +1,20 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
 func DNAtoRNA(dna string) string {
-
 	return strings.ReplaceAll(dna, "T", "U")
+}
+
+func Move(position, roll int) int {
+	return position + roll*2
+}
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+func AbbrevName(name string) string {
+	initials := strings.SplitAfter(name, " ")                                     //make a slice
+	return strings.ToUpper(string(initials[0][0]) + "." + string(initials[1][0])) // <- use this syntax to access the value of elements in a slice by index
 }
--- katas/katas_test.go
+++ katas/katas_test.go
@@ -5,10 +5,32 @@ import (
 )
 
 func TestDNAtoRNA(t *testing.T) {
-
 	got := DNAtoRNA("GCAT")
 	want := "GCAU"
+	assertString(t, got, want)
+}
+
+func TestMove(t *testing.T) {
+	got := Move(2, 6)
+	want := 14
+	assertInt(t, got, want)
+}
+
+func TestAbbrevName(t *testing.T) {
+	got := AbbrevName("fjord winstar")
+	want := "F.W"
+	assertString(t, got, want)
+}
+
+func assertString(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %v want %v", got, want)
+	}
+}
+func assertInt(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
 	}
 }
