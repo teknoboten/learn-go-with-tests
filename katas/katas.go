261ba11a690dd4b35341775178a0a3777e682e5e
serra <serra@boten.ca>
2023-04-20 10:17:50 -0700

    add invert kata

--- katas/katas.go
+++ katas/katas.go
@@ -18,3 +18,12 @@ func AbbrevName(name string) string {
 	initials := strings.SplitAfter(name, " ")                                     //make a slice
 	return strings.ToUpper(string(initials[0][0]) + "." + string(initials[1][0])) // <- use this syntax to access the value of elements in a slice by index
 }
+
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+func Invert(arr []int) []int {
+	var result []int
+	for _, v := range arr {
+		result = append(result, -v)
+	}
+	return result
+}
--- katas/katas_test.go
+++ katas/katas_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -20,6 +21,19 @@ func TestAbbrevName(t *testing.T) {
 	got := AbbrevName("fjord winstar")
 	want := "F.W"
 	assertString(t, got, want)
+}
+
+func TestInvert(t *testing.T) {
+	got := Invert([]int{1, 2, 3, 4, 5})
+	want := []int{-1, -2, -3, -4, -5}
+	assertSlice(t, got, want)
+}
+
+func assertSlice(t testing.TB, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) { // <- use reflect.deepEqual when output is a slice / array
+		t.Errorf("got %v want %v", got, want)
+	}
 }
 
 func assertString(t testing.TB, got, want string) {
